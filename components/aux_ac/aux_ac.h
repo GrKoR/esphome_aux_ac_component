b777730d70ad38922cfec487c50d638fa45c9acc
Brokly <brokly@mail.ru>
2022-05-27 07:46:22 +0300

    Исправлена фильтрация внешней температуры

--- components/aux_ac/aux_ac.h
+++ components/aux_ac/aux_ac.h
@@ -1314,10 +1314,10 @@ class AirCon : public esphome::Component, public esphome::climate::Climate {
                             // temp = big_info_body->outdoor_temperature - 0x20;
                             // фильтруем простейшим фильтром OUTDOOR_FILTER_PESCENT - взнос одного измерения в процентах
                             {
-                                const float koef = ((float)OUTDOOR_FILTER_PESCENT)/100;
-                                const float antkoef = 1.0 - koef;
-                                static float temp = _current_ac_state.temp_outdoor;
-                                temp = temp * antkoef + koef * (big_info_body->outdoor_temperature - 0x20);
+                                const float koef = ((float)OUTDOOR_FILTER_PESCENT);
+                                const float antkoef = 100 - koef;
+                                static float temp = _current_ac_state.temp_outdoor*100;
+                                temp = (temp * antkoef + koef * (big_info_body->outdoor_temperature - 0x20))/100;
                                 stateChangedFlag = stateChangedFlag || (_current_ac_state.temp_outdoor != temp);
                                 _current_ac_state.temp_outdoor = temp;
                             }
