18edab9742274e0451352caa77b643be650043f5
GrKoR <dr.g.kornev@gmail.com>
2022-07-14 17:22:57 +0300

    cade formatting

--- components/aux_ac/automation.h
+++ components/aux_ac/automation.h
@@ -1,172 +1,158 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/core/automation.h"
 #include "aux_ac.h"
+#include "esphome/core/automation.h"
+#include "esphome/core/component.h"
 
 namespace esphome {
 namespace aux_ac {
 
 // **************************************** DISPLAY ACTIONS ****************************************
-    template <typename... Ts>
-    class AirConDisplayOffAction : public Action<Ts...>
-    {
-    public:
-        explicit AirConDisplayOffAction(AirCon *ac) : ac_(ac) {}
+template <typename... Ts>
+class AirConDisplayOffAction : public Action<Ts...> {
+   public:
+    explicit AirConDisplayOffAction(AirCon *ac) : ac_(ac) {}
 
-        void play(Ts... x) override { this->ac_->displayOffSequence(); }
+    void play(Ts... x) override { this->ac_->displayOffSequence(); }
 
-    protected:
-        AirCon *ac_;
-    };
+   protected:
+    AirCon *ac_;
+};
 
-    template <typename... Ts>
-    class AirConDisplayOnAction : public Action<Ts...>
-    {
-    public:
-        explicit AirConDisplayOnAction(AirCon *ac) : ac_(ac) {}
+template <typename... Ts>
+class AirConDisplayOnAction : public Action<Ts...> {
+   public:
+    explicit AirConDisplayOnAction(AirCon *ac) : ac_(ac) {}
 
-        void play(Ts... x) override { this->ac_->displayOnSequence(); }
+    void play(Ts... x) override { this->ac_->displayOnSequence(); }
 
-    protected:
-        AirCon *ac_;
-    };
-
-
+   protected:
+    AirCon *ac_;
+};
 
 // **************************************** VERTICAL LOUVER ACTIONS ****************************************
-    template <typename... Ts>
-    class AirConVLouverSwingAction : public Action<Ts...>
-    {
-    public:
-        explicit AirConVLouverSwingAction(AirCon *ac) : ac_(ac) {}
+template <typename... Ts>
+class AirConVLouverSwingAction : public Action<Ts...> {
+   public:
+    explicit AirConVLouverSwingAction(AirCon *ac) : ac_(ac) {}
 
-        void play(Ts... x) override { this->ac_->setVLouverSwingSequence(); }
+    void play(Ts... x) override { this->ac_->setVLouverSwingSequence(); }
 
-    protected:
-        AirCon *ac_;
-    };
+   protected:
+    AirCon *ac_;
+};
 
-    template <typename... Ts>
-    class AirConVLouverStopAction : public Action<Ts...>
-    {
-    public:
-        explicit AirConVLouverStopAction(AirCon *ac) : ac_(ac) {}
+template <typename... Ts>
+class AirConVLouverStopAction : public Action<Ts...> {
+   public:
+    explicit AirConVLouverStopAction(AirCon *ac) : ac_(ac) {}
 
-        void play(Ts... x) override { this->ac_->setVLouverStopSequence(); }
+    void play(Ts... x) override { this->ac_->setVLouverStopSequence(); }
 
-    protected:
-        AirCon *ac_;
-    };
+   protected:
+    AirCon *ac_;
+};
 
-    template <typename... Ts>
-    class AirConVLouverTopAction : public Action<Ts...>
-    {
-    public:
-        explicit AirConVLouverTopAction(AirCon *ac) : ac_(ac) {}
+template <typename... Ts>
+class AirConVLouverTopAction : public Action<Ts...> {
+   public:
+    explicit AirConVLouverTopAction(AirCon *ac) : ac_(ac) {}
 
-        void play(Ts... x) override { this->ac_->setVLouverTopSequence(); }
+    void play(Ts... x) override { this->ac_->setVLouverTopSequence(); }
 
-    protected:
-        AirCon *ac_;
-    };
+   protected:
+    AirCon *ac_;
+};
 
-    template <typename... Ts>
-    class AirConVLouverMiddleAboveAction : public Action<Ts...>
-    {
-    public:
-        explicit AirConVLouverMiddleAboveAction(AirCon *ac) : ac_(ac) {}
+template <typename... Ts>
+class AirConVLouverMiddleAboveAction : public Action<Ts...> {
+   public:
+    explicit AirConVLouverMiddleAboveAction(AirCon *ac) : ac_(ac) {}
 
-        void play(Ts... x) override { this->ac_->setVLouverMiddleAboveSequence(); }
+    void play(Ts... x) override { this->ac_->setVLouverMiddleAboveSequence(); }
 
-    protected:
-        AirCon *ac_;
-    };
+   protected:
+    AirCon *ac_;
+};
 
-    template <typename... Ts>
-    class AirConVLouverMiddleAction : public Action<Ts...>
-    {
-    public:
-        explicit AirConVLouverMiddleAction(AirCon *ac) : ac_(ac) {}
+template <typename... Ts>
+class AirConVLouverMiddleAction : public Action<Ts...> {
+   public:
+    explicit AirConVLouverMiddleAction(AirCon *ac) : ac_(ac) {}
 
-        void play(Ts... x) override { this->ac_->setVLouverMiddleSequence(); }
+    void play(Ts... x) override { this->ac_->setVLouverMiddleSequence(); }
 
-    protected:
-        AirCon *ac_;
-    };
+   protected:
+    AirCon *ac_;
+};
 
-    template <typename... Ts>
-    class AirConVLouverMiddleBelowAction : public Action<Ts...>
-    {
-    public:
-        explicit AirConVLouverMiddleBelowAction(AirCon *ac) : ac_(ac) {}
+template <typename... Ts>
+class AirConVLouverMiddleBelowAction : public Action<Ts...> {
+   public:
+    explicit AirConVLouverMiddleBelowAction(AirCon *ac) : ac_(ac) {}
 
-        void play(Ts... x) override { this->ac_->setVLouverMiddleBelowSequence(); }
+    void play(Ts... x) override { this->ac_->setVLouverMiddleBelowSequence(); }
 
-    protected:
-        AirCon *ac_;
-    };
+   protected:
+    AirCon *ac_;
+};
 
-    template <typename... Ts>
-    class AirConVLouverBottomAction : public Action<Ts...>
-    {
-    public:
-        explicit AirConVLouverBottomAction(AirCon *ac) : ac_(ac) {}
+template <typename... Ts>
+class AirConVLouverBottomAction : public Action<Ts...> {
+   public:
+    explicit AirConVLouverBottomAction(AirCon *ac) : ac_(ac) {}
 
-        void play(Ts... x) override { this->ac_->setVLouverBottomSequence(); }
+    void play(Ts... x) override { this->ac_->setVLouverBottomSequence(); }
 
-    protected:
-        AirCon *ac_;
-    };
+   protected:
+    AirCon *ac_;
+};
 
-    template<typename... Ts>
-    class AirConVLouverSetAction : public Action<Ts...>
-    {
-    public:
-        AirConVLouverSetAction(AirCon *ac) : ac_(ac) {}
-        TEMPLATABLE_VALUE(uint8_t, value);
+template <typename... Ts>
+class AirConVLouverSetAction : public Action<Ts...> {
+   public:
+    AirConVLouverSetAction(AirCon *ac) : ac_(ac) {}
+    TEMPLATABLE_VALUE(uint8_t, value);
 
-        void play(Ts... x) {
-            vlpos_ = this->value_.value(x...);
-            this->ac_->setVLouverFrontendSequence( (ac_vlouver_frontend)vlpos_);
-        }
+    void play(Ts... x) {
+        vlpos_ = this->value_.value(x...);
+        this->ac_->setVLouverFrontendSequence((ac_vlouver_frontend)vlpos_);
+    }
 
-    protected:
-        AirCon *ac_;
-        uint8_t vlpos_;
-    };
-
+   protected:
+    AirCon *ac_;
+    uint8_t vlpos_;
+};
 
 // **************************************** SEND TEST PACKET ACTION ****************************************
-    template <typename... Ts>
-    class AirConSendTestPacketAction : public Action<Ts...>
-    {
-    public:
-        explicit AirConSendTestPacketAction(AirCon *ac) : ac_(ac) {}
-        void set_data_template(std::function<std::vector<uint8_t>(Ts...)> func) {
-            this->data_func_ = func;
-            this->static_ = false;
-        }        
-        void set_data_static(const std::vector<uint8_t> &data) {
-            this->data_static_ = data;
-            this->static_ = true;
+template <typename... Ts>
+class AirConSendTestPacketAction : public Action<Ts...> {
+   public:
+    explicit AirConSendTestPacketAction(AirCon *ac) : ac_(ac) {}
+    void set_data_template(std::function<std::vector<uint8_t>(Ts...)> func) {
+        this->data_func_ = func;
+        this->static_ = false;
+    }
+    void set_data_static(const std::vector<uint8_t> &data) {
+        this->data_static_ = data;
+        this->static_ = true;
+    }
+
+    void play(Ts... x) override {
+        if (this->static_) {
+            this->ac_->sendTestPacket(this->data_static_);
+        } else {
+            auto val = this->data_func_(x...);
+            this->ac_->sendTestPacket(val);
         }
+    }
 
-        void play(Ts... x) override {
-            if (this->static_) {
-                this->ac_->sendTestPacket(this->data_static_);
-            } else {
-                auto val = this->data_func_(x...);
-                this->ac_->sendTestPacket(val);
-            }
-        }
+   protected:
+    AirCon *ac_;
+    bool static_{false};
+    std::function<std::vector<uint8_t>(Ts...)> data_func_{};
+    std::vector<uint8_t> data_static_{};
+};
 
-    protected:
-        AirCon *ac_;
-        bool static_{false};
-        std::function<std::vector<uint8_t>(Ts...)> data_func_{};
-        std::vector<uint8_t> data_static_{};
-    };
-
-} // namespace aux_ac
-} // namespace esphome
+}  // namespace aux_ac
+}  // namespace esphome
