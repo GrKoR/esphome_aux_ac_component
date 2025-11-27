5a165d3a3dcd2eb65cf65434cab166bd906f9153
GrKoR <dr.g.kornev@gmail.com>
2025-11-26 18:58:36 -0800

    upd: version++

--- components/aux_ac/climate.py
+++ components/aux_ac/climate.py
@@ -33,7 +33,7 @@ from esphome.components.climate import (
     ClimateSwingMode,
 )
 
-AUX_AC_FIRMWARE_VERSION = '0.2.17'
+AUX_AC_FIRMWARE_VERSION = '0.3.1'
 AC_PACKET_TIMEOUT_MIN = 150
 AC_PACKET_TIMEOUT_MAX = 600
 AC_POWER_LIMIT_MIN = 30
